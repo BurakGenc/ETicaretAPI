4717fb63b0eb7a61c70de5cc70940ac0b8c3143a
BurakGenc <burakgenc14@gmail.com>
2022-09-07 04:21:12 +0300

    Tracking Performance Optimization

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -20,14 +20,17 @@ namespace ETicaretAPI.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-          await  _prodctWriteRepository.AddRangeAsync(new List<Product>{
-                new Product{Id=Guid.NewGuid(),Name="Product 1", Price=100,CreatedTime=DateTime.UtcNow,Stock=10},
-                new Product{Id=Guid.NewGuid(),Name="Product 2", Price=200,CreatedTime=DateTime.UtcNow,Stock=20},
-                new Product{Id=Guid.NewGuid(),Name="Product 3", Price=300,CreatedTime=DateTime.UtcNow,Stock=130},
+            //await  _prodctWriteRepository.AddRangeAsync(new List<Product>{
+            //      new Product{Id=Guid.NewGuid(),Name="Product 1", Price=100,CreatedTime=DateTime.UtcNow,Stock=10},
+            //      new Product{Id=Guid.NewGuid(),Name="Product 2", Price=200,CreatedTime=DateTime.UtcNow,Stock=20},
+            //      new Product{Id=Guid.NewGuid(),Name="Product 3", Price=300,CreatedTime=DateTime.UtcNow,Stock=130},
 
-            });
-          await  _prodctWriteRepository.SaveAsync();
-            
+            //  });
+            //await  _prodctWriteRepository.SaveAsync();
+
+          Product p=  await _productReadRepository.GetByIdAsync("9e57045a-9a68-4cb3-a036-3b59f015aa08");
+            p.Name = "Ahmet";
+            _prodctWriteRepository.SaveAsync();
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(string id)
